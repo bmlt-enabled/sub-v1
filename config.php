62bb545b6138c18fb48514cedf4e23d53d80eef1
Danny Gershman <danny.gershman@gmail.com>
2019-05-27 22:02:26 -0400

    adding config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+static $mysql_hostname = "";
+static $mysql_username = "";
+static $mysql_password = "";
+static $mysql_database = "";
+static $subscribe_keyword = "";
+static $unsubscribe_keyword = "";
+static $twilio_account_sid = "";
+static $twilio_auth_token = "";
